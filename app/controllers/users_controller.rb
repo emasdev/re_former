7045d03533eae919e2a7256974f9752e42bbc645
MarilenaRoque <marilenas@id.uff.br>
2020-08-19 14:54:13 -0300

    Create form and set the controllers
    Created an html form to create new users
    Set up params method to receive the user params
    Set a hash to retrieve all the user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
 
-  def create; end
+  end
+
+ 
+  def create
+    #@user = User.new(username: params[:user][:username], email: parms[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
+  
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
