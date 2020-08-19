b58ba88dc490fc0690e66f1fbc5aacee36b985e0
Emas <emas.dev@gmail.com>
2020-08-19 15:40:44 -0500

    Update user/new form
    - change user/new form using form_tag helper
    - change user/new form using form_for helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
- 
   def create
-    #@user = User.new(username: params[:user][:username], email: parms[:user][:email], password: params[:user][:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_user_path
     else
@@ -20,5 +19,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
 end
